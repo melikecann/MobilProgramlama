9898cf5127439e1ae11df6faffeec0a9c76ecf77
Melike Can <143530117+melikecann@users.noreply.github.com>
2025-03-10 10:18:36 +0300

    10.03.2025

---
+++ ikiEkran/app/src/main/java/com/example/ikiekran/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ikiekran;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button button2 = findViewById(R.id.button1);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
